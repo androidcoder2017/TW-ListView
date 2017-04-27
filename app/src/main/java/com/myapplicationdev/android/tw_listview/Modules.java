9c30eaeea1696c4e93018594162e1d391f53a044
15056112 <15056112@myrp.edu.sg>
2017-04-27 14:07:45 +0800

    Test

---
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/Modules.java
@@ -0,0 +1,24 @@
+package com.myapplicationdev.android.tw_listview;
+
+/**
+ * Created by 15056112 on 27/4/2017.
+ */
+
+public class Modules {
+    private String modulesCode;
+    private boolean prog;
+
+    public Modules(String modulesCode, boolean prog) {
+        this.modulesCode = modulesCode;
+        this.prog = prog;
+
+    }
+
+    public String getModulesCode(){
+        return modulesCode;
+    }
+
+    public boolean isProg(){
+        return prog;
+    }
+}
--- app/src/main/java/com/myapplicationdev/android/tw_listview/SecondActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/SecondActivity.java
@@ -3,13 +3,15 @@ package com.myapplicationdev.android.tw_listview;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class SecondActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
-    ListView lv;
-    TextView tvYear;
+public class SecondActivity extends AppCompatActivity {
 
 
     @Override
@@ -17,13 +19,33 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        ListView lv;
+        ArrayAdapter aa;
+        final ArrayList<Modules> modules;
+
+
         lv = (ListView) this.findViewById(R.id.lvModules);
-        tvYear = (TextView) findViewById(R.id.tvYear);
+
 
         Intent i = getIntent();
         String year = i.getStringExtra("year");
-        tvYear.setText(year);
 
+
+        modules = new ArrayList<Modules>();
+        modules.add(new Modules("C105", true));
+        modules.add(new Modules("C111", false));
+        modules.add(new Modules("C207", true));
+
+        modules.add(new Modules("C346", true));
+        modules.add(new Modules("C203", true));
+        modules.add(new Modules("C235", false));
+
+        modules.add(new Modules("C302", true));
+        modules.add(new Modules("C347", true));
+        modules.add(new Modules("C349", true));
+
+        aa = new ModuleAdapter(this, R.layout.row, modules);
+        lv.setAdapter(aa);
 
 
     }
